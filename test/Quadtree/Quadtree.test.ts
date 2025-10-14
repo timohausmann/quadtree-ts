bfb35afd84e4dccc8e0af1f535a94e7731b1d694
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-10-14 17:21:19 -0400

    updated code and types to allow Quadtree class to be extended in a subclass

--- test/Quadtree/Quadtree.test.ts
+++ test/Quadtree/Quadtree.test.ts
@@ -1,4 +1,10 @@
-import { Quadtree } from '../../src/Quadtree';
+import { Quadtree, QuadtreeProps } from '../../src/Quadtree';
+import type { Rectangle } from '../../src/Rectangle';
+import type { Circle } from '../../src/Circle';
+import type { Line } from '../../src/Line';
+import type { Indexable } from '../../src/types';
+import { expectTypeOf } from 'expect-type';
+type ObjectsTypeDefault = Rectangle|Circle|Line|Indexable
 
 describe('Quadtree.constructor', () => {
 
@@ -12,6 +18,8 @@ describe('Quadtree.constructor', () => {
         expect(tree.level).toBe(0);
         expect(tree.objects).toEqual([]);
         expect(tree.nodes).toEqual([]);
+
+        expectTypeOf(tree.nodes).toEqualTypeOf<Quadtree<ObjectsTypeDefault>[]>();
     });
 
     test('applies all arguments', () => {
@@ -21,6 +29,27 @@ describe('Quadtree.constructor', () => {
         expect(tree.bounds).toEqual({ x: 20, y: 40, width: 100, height: 200 });
         expect(tree.maxObjects).toBe(5);
         expect(tree.maxLevels).toBe(3);
+    });
+
+    test('handles subclass extension', () => {
+        class SubQuadTree<ObjectsType extends ObjectsTypeDefault> extends Quadtree<ObjectsType> {
+            public isSubClass: boolean
+
+            constructor(props:QuadtreeProps, level=0) {
+                super(props, level);
+                this.isSubClass = true;
+            }
+        }
+
+        const tree = new SubQuadTree({ x: 20, y: 40, width: 100, height: 200, maxObjects: 5, maxLevels: 3 });
+        tree.split();
+        expectTypeOf(tree.nodes).toEqualTypeOf<SubQuadTree<ObjectsTypeDefault>[]>();
+
+        for(const node of tree.nodes){
+            expect(node).toBeInstanceOf(SubQuadTree);
+            expect(node.isSubClass).toBe(true);
+            expectTypeOf(node).toEqualTypeOf<SubQuadTree<ObjectsTypeDefault>>();
+        }
     });
 });
 
