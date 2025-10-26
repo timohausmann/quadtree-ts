2330710cc108e029b77c3c690b243a2cc505f43a
Timo Hausmann <hello.timohausmann@gmail.com>
2025-10-26 15:15:57 +0100

    update dev dependencies, add prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsdoc from "eslint-plugin-tsdoc";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        tsdoc,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "no-console": "error",
+        "tsdoc/syntax": "warn",
+    },
+}]);
