0f5510571c7f0e6be32b3ef2ae71e42072cde466
Timo Hausmann <hello.timohausmann@gmail.com>
2025-10-27 19:15:22 +0100

    chore: optimize npm package
    - Remove src/ from published files (reduce package size)
    - Remove dist/ and types/ from git (build and published by CI)

--- types/index.esm.d.ts
+++
@@ -1,9 +0,0 @@
-export { Quadtree } from './Quadtree';
-export { Rectangle } from './Rectangle';
-export { Circle } from './Circle';
-export { Line } from './Line';
-export type { QuadtreeProps } from './Quadtree';
-export type { CircleProps, CircleGeometry } from './Circle';
-export type { LineProps, LineGeometry } from './Line';
-export type { RectangleProps, RectangleGeometry } from './Rectangle';
-export type { NodeGeometry, Indexable } from './types';
